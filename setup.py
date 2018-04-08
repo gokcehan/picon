a2078e341960c86edce98e228022cedf41aee2ac
Gokcehan <gokcehankara@gmail.com>
2018-04-08 16:47:52 +0300

    update python setup

--- setup.py
+++ setup.py
@@ -1,19 +1,9 @@
-from setuptools import setup, find_packages
-
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+from setuptools import setup
 
 setup(
     name='picon',
-    version='1.0.0',
+    version='1',
     description='Run your code in python interactive console from the command line',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
     url='https://github.com/gokcehan/picon',
     author='Gokcehan Kara',
     author_email='gokcehankara@gmail.com',
